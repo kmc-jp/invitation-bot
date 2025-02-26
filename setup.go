32c39c09204610e74ffa17a0cc0f09f6519d7531
wass80 <wasss80@gmail.com>
2025-02-26 22:28:27 +0900

    socket mode

--- setup.go
+++
@@ -1,7 +0,0 @@
-package invbot
-
-type configuration struct {
-	Token  string `json:"SLACK_TOKEN" required:"true"`
-	Secret string `json:"SLACK_SIGNING_SECRET" required:"true"`
-	Prefix string `json:"PREFIX_CHANNEL" required:"true"`
-}
